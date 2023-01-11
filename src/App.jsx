0eadab998bbe58eeabeecb724c0199385fffe64e
srujan-landeri <66351075+srujan-landeri@users.noreply.github.com>
2023-01-11 18:15:10 +0530

    1 file modified, 3 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
 
 export default function App() {
   return (
     <div className="">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
