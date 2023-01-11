31d0a7b66a8f0e5a362fee8017ec8ebbfb1bf209
srujan-landeri <66351075+srujan-landeri@users.noreply.github.com>
2023-01-11 19:31:54 +0530

    2 files modified

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Navigation() {
+  const location = useLocation().pathname.slice(1);
+  const navigate = useNavigate();
+
   return (
-    <nav className="navigation-bar display-flex">
+    <nav className="navigation-bar flex">
       <div>
         <img
           className="nav-logo"
           src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
           alt=""
+          onClick={() => navigate('/')}
         />
       </div>
       <div>
-        <ul className="nav-page-links display-flex">
-          <li>
-            <a href="">Home</a>
+        <ul className="nav-page-links flex">
+          <li
+            className={location === '' ? 'nav-active-link' : ''}
+            onClick={() => navigate('/')}
+          >
+            Home
           </li>
-          <li>
-            <a href="">Home</a>
+          <li
+            className={location === 'offers' ? 'nav-active-link' : ''}
+            onClick={() => navigate('/offers')}
+          >
+            Offers
           </li>
-          <li>
-            <a href="">Home</a>
+          <li
+            className={location === 'sign-in' ? 'nav-active-link' : ''}
+            onClick={() => navigate('/sign-in')}
+          >
+            Signin
           </li>
         </ul>
       </div>
